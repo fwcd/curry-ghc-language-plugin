7f0a0cae85648e33e08ea2616bc206d5f8b5e53b
fwcd <fwcdmail@gmail.com>
2021-04-26 16:14:18 +0200

    Update create syntax trans

--- src/Plugin/Trans/CreateSyntax.hs
+++ src/Plugin/Trans/CreateSyntax.hs
@@ -11,6 +11,9 @@ syntactic constructs for GHC's abstract syntax tree.
 -}
 module Plugin.Trans.CreateSyntax where
 
+import Language.Haskell.Syntax (noExtField, NoGhcTc)
+
+import GHC.Parser.Annotation (noLocA, noAnn, LocatedAn, NameAnn)
 import GHC.Plugins hiding (getSrcSpanM)
 import GHC.Hs.Binds
 import GHC.Hs.Extension
@@ -48,13 +51,13 @@ mkConLam _ mw c [] vs = do
           Just w  -> XExpr . WrapExpr . HsWrap w
           Nothing -> id
     -- Apply all variables in reverse to the constructor.
-    let e = foldl ((noLoc .) . HsApp noExtField)
-            (noLoc (wrap (HsConLikeOut noExtField (RealDataCon c))))
-            (map (noLoc . HsVar noExtField . noLoc) $ reverse vs)
+    let e = foldl ((noLocA .) . HsApp noAnn)
+            (noLocA (wrap (HsConLikeOut noExtField (RealDataCon c))))
+            (map (noLocA . HsVar noExtField . noLocA) $ reverse vs)
     -- Get the result type of the constructor.
     ty <- snd . splitFunTys <$> getTypeOrPanic e -- ok
     -- Wrap the whole term in a 'return'.
-    e' <- mkApp mkNewReturnTh ty [noLoc $ HsPar noExtField e]
+    e' <- mkApp mkNewReturnTh ty [noLocA $ HsPar noAnn e]
     mty <- mkTyConTy <$> getMonadTycon
     return (e', mkAppTy mty ty)
 -- Create lambdas for the remaining types.
@@ -74,19 +77,19 @@ mkConLam tcs w c ((Scaled _ ty, strictness) : tys) vs = do
       -- create the lambda-bound variable, that needs to be shared
       v' <- freshVar vty'
       -- create share
-      s <- mkApp (mkNewShareTh tcs) ty [noLoc (HsVar noExtField (noLoc v'))]
+      s <- mkApp (mkNewShareTh tcs) ty [noLocA (HsVar noExtField (noLocA v'))]
       mtycon <- getMonadTycon
       -- create seqValue
       seqE <- mkApp (mkNewSeqValueTh (bindingType ty)) (bindingType resty)
-                [noLoc (HsVar noExtField (noLoc v)), e]
-      let l = noLoc (HsPar noExtField (mkLam (noLoc v) vty' seqE resty))
+                [noLocA (HsVar noExtField (noLocA v)), e]
+      let l = noLocA (HsPar noAnn (mkLam (noLocA v) vty' seqE resty))
       let sty = mkTyConApp mtycon [ty]
-      shareE <- mkBind (noLoc (HsPar noExtField s)) sty l resty
+      shareE <- mkBind (noLocA (HsPar noAnn s)) sty l resty
       return (shareE, v')
   -- Make the lambda for this variable
-  let e'' = mkLam (noLoc v') (Scaled Many ty) e' resty
+  let e'' = mkLam (noLocA v') (Scaled Many ty) e' resty
   -- Wrap the whole term in a 'return'.
-  e''' <- mkApp mkNewReturnTh lamty [noLoc $ HsPar noExtField e'']
+  e''' <- mkApp mkNewReturnTh lamty [noLocA $ HsPar noAnn e'']
   let mty = mkTyConTy mtc
   return (e''', mkAppTy mty lamty)
 
@@ -94,8 +97,8 @@ mkConLam tcs w c ((Scaled _ ty, strictness) : tys) vs = do
 mkBindLam :: Scaled Type -> LHsExpr GhcTc -> TcM (LHsExpr GhcTc)
 mkBindLam (Scaled m ty) e1' = do
   let ty' = bindingType ty
-  v <- noLoc <$> freshVar (Scaled m ty')
-  let bdy = noLoc $ HsApp noExtField (noLoc (HsVar noExtField v)) e1'
+  v <- noLocA <$> freshVar (Scaled m ty')
+  let bdy = noLocA $ HsApp noAnn (noLocA (HsVar noExtField v)) e1'
   let (_ , _, resty) = splitFunTy (varType (unLoc v))
   return (mkLam v (Scaled m ty') bdy resty)
 
@@ -185,13 +188,13 @@ mkNewShareTh tcs ty
     let docImportant = "Cannot share polymorphic values."
     (ty', _) <- liftIO $ removeNondetShareable tcs mtc stc ty
     let docSuppl = "For a variable with type" <+> ppr ty'
-    msg <- mkErrDocAt sp (errDoc [docImportant] [docSuppl, errrInfo] [])
+    msg <- mkDecoratedSDocAt sp docImportant docSuppl errrInfo
     reportError msg
     let expType = mkVisFunTyMany ty $ -- a ->
                   mkTyConApp mtc [ty] -- m a
     u <- getUniqueM
     let nm = mkSystemName u $ mkVarOcc "share_hole"
-    return (noLoc $ HsVar noExtField $ noLoc $ mkVanillaGlobal nm expType)
+    return (noLocA $ HsVar noExtField $ noLocA $ mkVanillaGlobal nm expType)
   | otherwise     = do
   mtycon <- getMonadTycon
   ps_expr <- queryBuiltinFunctionName "shre"
@@ -278,7 +281,7 @@ mkListBind :: Type -> Type -> TcM SyntaxExprTc
 mkListBind a b = do
   (e, constraints) <- captureConstraints (mkApp mk b [])
   wrapper <- mkWpLet . EvBinds <$> simplifyTop constraints
-  res <- zonkTopLExpr (noLoc (mkHsWrap wrapper (unLoc e)))
+  res <- zonkTopLExpr (noLocA (mkHsWrap wrapper (unLoc e)))
   return (SyntaxExprTc (unLoc res) [WpHole, WpHole] WpHole)
   where
     mk _ = do
@@ -295,7 +298,7 @@ mkListReturn :: Type -> TcM SyntaxExprTc
 mkListReturn a = do
   (e, constraints) <- captureConstraints (mkApp mk a [])
   wrapper <- mkWpLet . EvBinds <$> simplifyTop constraints
-  res <- zonkTopLExpr (noLoc (mkHsWrap wrapper (unLoc e)))
+  res <- zonkTopLExpr (noLocA (mkHsWrap wrapper (unLoc e)))
   return (SyntaxExprTc (unLoc res) [WpHole] WpHole)
   where
     mk _ = do
@@ -308,7 +311,7 @@ mkListFail :: Type -> TcM SyntaxExprTc
 mkListFail a = do
   (e, constraints) <- captureConstraints (mkApp mk a [])
   wrapper <- mkWpLet . EvBinds <$> simplifyTop constraints
-  res <- zonkTopLExpr (noLoc (mkHsWrap wrapper (unLoc e)))
+  res <- zonkTopLExpr (noLocA (mkHsWrap wrapper (unLoc e)))
   return (SyntaxExprTc (unLoc res) [WpHole] WpHole)
   where
     mk _ = do
@@ -321,7 +324,7 @@ mkListGuard :: TcM SyntaxExprTc
 mkListGuard = do
   (e, constraints) <- captureConstraints (mkApp mk unitTy [])
   wrapper <- mkWpLet . EvBinds <$> simplifyTop constraints
-  res <- zonkTopLExpr (noLoc (mkHsWrap wrapper (unLoc e)))
+  res <- zonkTopLExpr (noLocA (mkHsWrap wrapper (unLoc e)))
   return (SyntaxExprTc (unLoc res) [WpHole] WpHole)
   where
     mk _ = do
@@ -334,7 +337,7 @@ mkListSeq :: Type -> Type -> TcM SyntaxExprTc
 mkListSeq a b = do
   (e, constraints) <- captureConstraints (mkApp mk b [])
   wrapper <- mkWpLet . EvBinds <$> simplifyTop constraints
-  res <- zonkTopLExpr (noLoc (mkHsWrap wrapper (unLoc e)))
+  res <- zonkTopLExpr (noLocA (mkHsWrap wrapper (unLoc e)))
   return (SyntaxExprTc (unLoc res) [WpHole, WpHole] WpHole)
   where
     mk _ = do
@@ -350,79 +353,79 @@ mkListSeq a b = do
 mkEmptyList :: Type -> TyConMap -> TcM (LHsExpr GhcTc)
 mkEmptyList ty tcs = do
   dc <- liftIO (getLiftedCon nilDataCon tcs)
-  return (noLoc (XExpr (WrapExpr (HsWrap (WpTyApp ty)
+  return (noLocA (XExpr (WrapExpr (HsWrap (WpTyApp ty)
     (HsConLikeOut noExtField (RealDataCon dc))))))
 
 -- | Create a lifted cons list constructor.
 mkConsList :: Type -> TyConMap -> TcM (LHsExpr GhcTc)
 mkConsList ty tcs = do
   dc <- liftIO (getLiftedCon consDataCon tcs)
-  return (noLoc (XExpr (WrapExpr (HsWrap (WpTyApp ty)
+  return (noLocA (XExpr (WrapExpr (HsWrap (WpTyApp ty)
     (HsConLikeOut noExtField (RealDataCon dc))))))
 
 
 -- | Create a general lambda that binds one variable on its left side.
-mkLam :: Located Id -> Scaled Type -> LHsExpr GhcTc -> Type -> LHsExpr GhcTc
+mkLam :: LocatedAn NameAnn Id -> Scaled Type -> LHsExpr GhcTc -> Type -> LHsExpr GhcTc
 mkLam v ty' bdy resty =
   let pat = VarPat noExtField v
-      grhs = GRHS noExtField ([] :: [GuardLStmt GhcTc]) bdy
-      rhs = GRHSs noExtField [noLoc grhs] (noLoc (EmptyLocalBinds noExtField))
-      match = Match noExtField LambdaExpr [noLoc pat] rhs
+      grhs = GRHS noAnn ([] :: [GuardLStmt GhcTc]) bdy
+      rhs = GRHSs noExtField [noLoc grhs] (EmptyLocalBinds noExtField)
+      match = Match noAnn LambdaExpr [noLocA pat] rhs
       mgtc = MatchGroupTc [ty'] resty
-      mg = MG mgtc (noLoc [noLoc match]) Generated
-  in noLoc $ HsPar noExtField $ noLoc $ HsLam noExtField mg
+      mg = MG mgtc (noLocA [noLocA match]) Generated
+  in noLocA $ HsPar noAnn $ noLocA $ HsLam noExtField mg
 
 -- | Create a simple let expression that binds the given variable to
 -- the first LHsExpr, where the other LHsExpr is used for the "in" of the let.
 mkSimpleLet :: RecFlag -> LHsExpr GhcTc -> LHsExpr GhcTc -> Var -> Type
             -> HsExpr GhcTc
 mkSimpleLet f scr e v a =
-  let grhs = GRHS noExtField [] scr
-      grhss = GRHSs noExtField [noLoc grhs] (noLoc (EmptyLocalBinds noExtField))
-      ctxt = FunRhs (noLoc (varName v)) Prefix NoSrcStrict
-      alt = Match noExtField ctxt [] grhss
+  let grhs = GRHS noAnn [] scr
+      grhss = GRHSs noExtField [noLoc grhs] (EmptyLocalBinds noExtField)
+      ctxt = FunRhs (noLocA (varName v)) Prefix NoSrcStrict
+      alt = Match noAnn ctxt [] grhss
       mgtc = MatchGroupTc [] a
-      mg = MG mgtc (noLoc [noLoc alt]) Generated
-      b = FunBind WpHole (noLoc v) mg []
-      nbs = NValBinds [(f, listToBag [noLoc b])] []
-      bs = HsValBinds noExtField (XValBindsLR nbs)
-  in HsLet noExtField (noLoc bs) e
+      mg = MG mgtc (noLocA [noLocA alt]) Generated
+      b = FunBind WpHole (noLocA v) mg []
+      nbs = NValBinds [(f, listToBag [noLocA b])] []
+      bs = HsValBinds noAnn (XValBindsLR nbs)
+  in HsLet noExtField bs e
 
 -- | Create a simple let expression that binds the given pattern to
 -- the first LHsExpr, where the other LHsExpr is used for the "in" of the let.
 mkSimplePatLet :: Type -> LHsExpr GhcTc -> LPat GhcTc -> LHsExpr GhcTc
                -> HsExpr GhcTc
 mkSimplePatLet ty scr p e =
-  let grhs = GRHS noExtField [] scr
-      grhss = GRHSs noExtField [noLoc grhs] (noLoc (EmptyLocalBinds noExtField))
+  let grhs = GRHS noAnn [] scr
+      grhss = GRHSs noExtField [noLoc grhs] (EmptyLocalBinds noExtField)
       b = PatBind ty p grhss ([], [[]])
-      nbs = NValBinds [(Recursive, listToBag [noLoc b])] []
-      bs = HsValBinds noExtField (XValBindsLR nbs)
-  in HsLet noExtField (noLoc bs) e
+      nbs = NValBinds [(Recursive, listToBag [noLocA b])] []
+      bs = HsValBinds noAnn (XValBindsLR nbs)
+  in HsLet noExtField bs e
 
 -- | Create a simple (case) alternative with the given right side and patterns.
 mkSimpleAlt :: HsMatchContext (NoGhcTc GhcTc) -> LHsExpr GhcTc -> [LPat GhcTc]
             -> Match GhcTc (LHsExpr GhcTc)
 mkSimpleAlt ctxt e ps =
-  let grhs = GRHS noExtField [] e
-      grhss = GRHSs noExtField [noLoc grhs] (noLoc (EmptyLocalBinds noExtField))
-  in Match noExtField ctxt ps grhss
+  let grhs = GRHS noAnn [] e
+      grhss = GRHSs noExtField [noLoc grhs] (EmptyLocalBinds noExtField)
+  in Match noAnn ctxt ps grhss
 
 -- | Create a variable pattern with the given parameter.
 mkVarPat :: Var -> LPat GhcTc
-mkVarPat v = noLoc (VarPat noExtField (noLoc v))
+mkVarPat v = noLocA (VarPat noExtField (noLocA v))
 
 -- | Creates a let statement for the given binding.
 toLetStmt :: (RecFlag, LHsBinds GhcTc) -> ExprLStmt GhcTc
-toLetStmt b = noLoc
-  (LetStmt noExtField (noLoc
-    (HsValBinds noExtField (XValBindsLR (NValBinds [b] [])))))
+toLetStmt b = noLocA
+  (LetStmt noAnn
+    (HsValBinds noAnn (XValBindsLR (NValBinds [b] []))))
 
 -- | Create a let with the given binding and inner expression.
 toLetExpr :: (RecFlag, LHsBinds GhcTc) -> LHsExpr GhcTc -> LHsExpr GhcTc
-toLetExpr b e = noLoc
-  (HsLet noExtField (noLoc
-    (HsValBinds noExtField (XValBindsLR (NValBinds [b] [])))) e)
+toLetExpr b e = noLocA
+  (HsLet noExtField
+    (HsValBinds noAnn (XValBindsLR (NValBinds [b] []))) e)
 
 mkHsWrap :: HsWrapper -> HsExpr GhcTc -> HsExpr GhcTc
 mkHsWrap WpHole e = e
